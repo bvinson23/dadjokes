f4219a054321cc8464b84871bf5c1cb46adfc0b0
bvinson23 <brandonvinson23@gmail.com>
2021-03-05 16:01:55 -0600

    trying to get the jokes to display

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,8 +1,9 @@
 export const getJokes = () => {
-    return fetch("https://icanhazdadjoke.com/",{
+    return fetch("https://icanhazdadjoke.com/", {
         method: "GET",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "User-Agent": "github.com/bvinson23/dadjokes"
         }
     })
     .then(response => response.json())
